7c80b247861bee29c0fc7b62ddf1fda8b3c19770
kazunori <matunorigmnw@gmail.com>
2021-01-10 10:18:32 +0000

    firts commit

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = function fizzbuzz(value) {
+    if (value % 15 === 0) {
+        return "FizzBuzz";
+    } else if (value % 3 === 0) {
+        return "Fizz";
+    } else if (value % 5 === 0) {
+        return "Buzz";
+    } else {
+        return String(value);
+    }
+};
