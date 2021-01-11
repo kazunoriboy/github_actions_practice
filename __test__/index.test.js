311396ad98502e50cc9f88520c61803af6e5c9f2
kazunori <matunorigmnw@gmail.com>
2021-01-11 04:01:14 +0000

    fix test

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -2,15 +2,15 @@ const fizzbuzz = require("../index");
 
 describe("fizzbuzz", () => {
     it("returns FizzBuzz when value is divisible by 15", () => {
-        expect(fizzbuzz(30)).toStrictEqual("ChageAsu");
+        expect(fizzbuzz(30)).toStrictEqual("FizzBuzz");
     });
 
     it("returns Fizz when value is divisibled by 3", () => {
-        expect(fizzbuzz(3)).toStrictEqual("Chage");
+        expect(fizzbuzz(3)).toStrictEqual("Fizz");
     });
 
     it("returns Buzz when value is divisibel by 5", () => {
-        expect(fizzbuzz(5)).toStrictEqual("Asu");
+        expect(fizzbuzz(5)).toStrictEqual("Buzz");
     });
 
     it("returns the value when value is not divisible by 3 or 5", () => {
