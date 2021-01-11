1aec18ee6dccde296a196fcd3aa99cfb0568df54
kazunori <matunorigmnw@gmail.com>
2021-01-11 03:59:19 +0000

    fail test

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -2,15 +2,15 @@ const fizzbuzz = require("../index");
 
 describe("fizzbuzz", () => {
     it("returns FizzBuzz when value is divisible by 15", () => {
-        expect(fizzbuzz(30)).toStrictEqual("FizzBuzz");
+        expect(fizzbuzz(30)).toStrictEqual("ChageAsu");
     });
 
     it("returns Fizz when value is divisibled by 3", () => {
-        expect(fizzbuzz(3)).toStrictEqual("Fizz");
+        expect(fizzbuzz(3)).toStrictEqual("Chage");
     });
 
     it("returns Buzz when value is divisibel by 5", () => {
-        expect(fizzbuzz(5)).toStrictEqual("Buzz");
+        expect(fizzbuzz(5)).toStrictEqual("Asu");
     });
 
     it("returns the value when value is not divisible by 3 or 5", () => {
